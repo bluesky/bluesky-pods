93bd5ccd7c713f7bf38ad70a043a3fee3ec10c1b
Thomas A Caswell <tcaswell@bnl.gov>
2020-07-30 18:00:15 -0400

    ENH: make IPython history persistent and make use of profile startup

--- launch_bluesky.sh
+++ launch_bluesky.sh
@@ -4,4 +4,4 @@ set -o xtrace
 
 xhost +local:docker
 
-podman run --pod adaptive -ti -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY -v `pwd`:'/app' -w '/app' --rm bluesky ipython3 -i fake_profile.py
+podman run --pod adaptive -ti -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY -v `pwd`:'/app' -w '/app' -v ./bluesky_config/ipython:/etc/ipython  --rm bluesky ipython3 --ipython-dir=/etc/ipython
