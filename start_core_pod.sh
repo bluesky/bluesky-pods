15745df1adaeb21a13fec12d2b3296861417d24d
Marcus D. Hanwell <mhanwell@bnl.gov>
2020-08-06 16:35:37 -0400

    Rename pod from adaptive to acquisition
    Simple rename, follow on commits will add more focused pods outside of
    the acquisition activity.

--- start_core_pod.sh
+++ start_core_pod.sh
@@ -3,28 +3,28 @@ set -e
 set -o xtrace
 
 # create the pod
-podman pod create -n adaptive -p 60606:8081/tcp -p 9092:9092/tcp -p 29092:29092/tcp -p 6977:6669/tcp
+podman pod create -n acquisition -p 60606:8081/tcp -p 9092:9092/tcp -p 29092:29092/tcp -p 6977:6669/tcp
 # just to get minimal IOC running
-podman run -dt --pod adaptive --rm caproto
+podman run -dt --pod acquisition --rm caproto
 
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.mini_beamline -v --interfaces=127.0.0.1
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1 --prefix="random_walk:horiz-"
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1 --prefix="random_walk:vert-"
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.simple -v --interfaces=127.0.0.1
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.thermo_sim -v --interfaces=127.0.0.1
-podman run -dt --pod adaptive --rm caproto python3 -m caproto.ioc_examples.trigger_with_pc -v --interfaces=127.0.0.1
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.mini_beamline -v --interfaces=127.0.0.1
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1 --prefix="random_walk:horiz-"
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.random_walk -v --interfaces=127.0.0.1 --prefix="random_walk:vert-"
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.simple -v --interfaces=127.0.0.1
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.thermo_sim -v --interfaces=127.0.0.1
+podman run -dt --pod acquisition --rm caproto python3 -m caproto.ioc_examples.trigger_with_pc -v --interfaces=127.0.0.1
 # start up a mongo
-podman run -dt --pod adaptive --rm mongo
+podman run -dt --pod acquisition --rm mongo
 # stort up redis
-podman run -dt --pod adaptive  --rm redis
+podman run -dt --pod acquisition  --rm redis
 # start up a zmq proxy
-podman run --pod adaptive -dt --rm  bluesky bluesky-0MQ-proxy 4567 5678
+podman run --pod acquisition -dt --rm  bluesky bluesky-0MQ-proxy 4567 5678
 
 # set up kafka
-podman run --pod adaptive -dt -e ALLOW_ANONYMOUS_LOGIN=yes -v /bitnami  --rm bitnami/zookeeper:3
+podman run --pod acquisition -dt -e ALLOW_ANONYMOUS_LOGIN=yes -v /bitnami  --rm bitnami/zookeeper:3
 
-podman run --pod adaptive \
+podman run --pod acquisition \
        -dt --rm \
        -e KAFKA_CFG_ZOOKEEPER_CONNECT=localhost:2181   \
        -e ALLOW_PLAINTEXT_LISTENER=yes \
@@ -36,7 +36,7 @@ podman run --pod adaptive \
        bitnami/kafka:2
 
 # start up queueserver
-podman run --pod adaptive -td --rm bluesky python3 -m aiohttp.web -H localhost -P 8081 bluesky_queueserver.server:init_func
+podman run --pod acquisition -td --rm bluesky python3 -m aiohttp.web -H localhost -P 8081 bluesky_queueserver.server:init_func
 
 # start up databroker server
-podman run --pod adaptive -dt --rm databroker-server uvicorn --port 6669 databroker_server.main:app
+podman run --pod acquisition -dt --rm databroker-server uvicorn --port 6669 databroker_server.main:app
