7b1b142852dcfda52765fc60f2c92a610b650f22
Marcus D. Hanwell <mhanwell@bnl.gov>
2020-08-06 09:46:55 -0400

    Build the databroker server image using buildah

---
+++ build_databroker_server_image.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+set -e
+set -o xtrace
+
+
+container=$(buildah from bluesky-base)
+buildah run $container -- pip3 install git+https://github.com/bluesky/databroker-server.git@master#egg=databroker-server
+
+buildah unmount $container
+buildah commit $container databroker-server
