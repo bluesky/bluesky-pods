ea97c50b076770ba30c1176c90d764facd0425f8
Thomas A Caswell <tcaswell@bnl.gov>
2020-08-14 11:23:30 -0400

    ENH: make launch_bluesky image configurable

--- launch_bluesky.sh
+++ launch_bluesky.sh
@@ -4,6 +4,12 @@ set -o xtrace
 
 xhost +local:docker
 
+if [ "$1" != "" ]; then
+    imagename=$1
+else
+    imagename="bluesky"
+fi
+
 podman run --pod acquisition \
        -ti  --rm \
        -v /tmp/.X11-unix/:/tmp/.X11-unix/ -e DISPLAY \
@@ -11,5 +17,5 @@ podman run --pod acquisition \
        -v ./bluesky_config/ipython:/usr/local/share/ipython \
        -v ./bluesky_config/databroker:/usr/local/share/intake \
        -v ./bluesky_config/happi:/usr/local/share/happi \
-       bluesky \
+       $imagename \
        ipython3 --ipython-dir=/usr/local/share/ipython
