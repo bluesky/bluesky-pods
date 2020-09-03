e389dcb9506ed38de8151c91de622449d0087afe
Dan Allan <dallan@bnl.gov>
2020-09-02 22:29:23 -0400

    Only set DISPLAY if we are in an SSH session.

--- launch_bluesky.sh
+++ launch_bluesky.sh
@@ -2,14 +2,17 @@
 set -e
 set -o xtrace
 
-# See this for xauth stuff.
+# This was a useful reference for the $DISPLAY and xauth stuff.
 # https://stackoverflow.com/a/48235281/1221924
 
-# Unlike the recommendation in the linked SO post,
-# we are not using docker and thus not using docker's special network.
-# Instead, we use the IP of the host.
-IP_ADDR=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-DISPLAY=`echo $DISPLAY | sed "s/^[^:]*\(.*\)/${IP_ADDR}\1/"`
+if [ -v "SSH_CONNECTION" ]; then
+	echo "SSH_CONNECTION is set"
+	# Unlike the recommendation in the linked SO post,
+	# we are not using docker and thus not using docker's special network.
+	# Instead, we use the IP of the host.
+	IP_ADDR=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+	DISPLAY=`echo $DISPLAY | sed "s/^[^:]*\(.*\)/${IP_ADDR}\1/"`
+fi
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
