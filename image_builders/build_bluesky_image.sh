4298641d4215a7ff01653a73f1a7babcab25c781
Jun Aishima <jaishima@bnl.gov>
2020-09-22 11:47:52 -0400

    update branch name on bluesky-queueserver to main

--- image_builders/build_bluesky_image.sh
+++ image_builders/build_bluesky_image.sh
@@ -6,7 +6,7 @@ set -o xtrace
 container=$(buildah from bluesky-base)
 buildah run $container -- pip3 install nslsii
 buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-adaptive.git@master#egg=bluesky-adaptive
-buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-queueserver.git@master#egg=bluesky-queueserver
+buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-queueserver.git@main#egg=bluesky-queueserver
 buildah run $container -- pip3 install git+https://github.com/pcdshub/happi.git@master#egg=happi
 
 buildah run $container -- pip3 uninstall --yes pyepics
