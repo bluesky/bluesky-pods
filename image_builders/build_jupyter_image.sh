21af9274017472e891c35f0a8bff0a5603075ffe
Dan Allan <dallan@bnl.gov>
2020-10-27 09:54:08 -0400

    Add server and lab extensions.

--- image_builders/build_jupyter_image.sh
+++ image_builders/build_jupyter_image.sh
@@ -4,8 +4,15 @@ set -o xtrace
 
 
 container=$(buildah from bluesky)
-buildah run $container -- pip3 install jupyterlab voila
+buildah run $container -- python3 -c "import matplotlib; matplotlib.use('Agg'); import matplotlib.pyplot"  # Build font cache.
+buildah run $container -- pip3 install jupyterlab voila papermill
+buildah run $container -- pip3 install git+https://github.com/danielballan/papermillhub
 buildah run $container -- jupyter serverextension enable voila --sys-prefix
-buildah config --cmd "jupyter lab --no-browser --allow-root --NotebookApp.token=''" $container
+buildah run $container -- jupyter serverextension enable papermillhub.nbext --sys-prefix
+buildah run $container -- dnf install -y nodejs
+buildah run $container -- jupyter labextension install @jupyter-widgets/jupyterlab-manager
+buildah run $container -- jupyter labextension install jupyter-matplotlib
+buildah run $container -- jupyter labextension install @jupyter-voila/jupyterlab-preview
+buildah config --cmd "jupyter lab --no-browser --allow-root --NotebookApp.token='dev'" $container
 buildah unmount $container
 buildah commit $container jupyter
