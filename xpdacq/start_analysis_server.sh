53500624d8e30f1407c7a26b9a3c08d60061ad9e
Thomas A Caswell <tcaswell@bnl.gov>
2020-12-03 23:50:52 -0500

    ENH: make work with raw data from last TiCu experiment

--- xpdacq/start_analysis_server.sh
+++ xpdacq/start_analysis_server.sh
@@ -7,6 +7,7 @@ podman run --pod acquisition \
        --rm -ti \
        -v ./bluesky_config/xpdacq/acq:/etc/acq \
        -v ./bluesky_config/databroker:/root/.local/share/intake \
-       -v /mnt/store/data_cache/:/data \
+       -v /mnt/store/data_cache/omnia:/data/omnia \
+       -v /mnt/data/bnl/ae_exfil:/data/ae_exfil \
        xpdan \
        bash -c "/opt/conda/envs/bluesky/bin/analysis_server"
