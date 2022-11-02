bad9bd4c346b9cc74c620ef4e7d39cbe48e044df
Thomas A Caswell <tcaswell@bnl.gov>
2022-11-02 12:03:05 -0400

    ENH: place dummy kafka config where nslsii expects to find it

--- image_builders/build_bluesky_image.sh
+++ image_builders/build_bluesky_image.sh
@@ -10,6 +10,8 @@ buildah run $container -- pip3 install git+https://github.com/bluesky/bluesky-qu
 buildah run $container -- pip3 install git+https://github.com/pcdshub/happi.git@master#egg=happi
 
 buildah run $container -- pip3 uninstall --yes pyepics
-
+buildah run $container -- mkdir /etc/bluesky
+buildah copy $container image_builders/kafka.yml /etc/bluesky/kafka.yml
 buildah unmount $container
+
 buildah commit $container bluesky
