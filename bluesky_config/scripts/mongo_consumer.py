629848ed5e4b2759d1e233f4299e5c2fb35642ec
Thomas A Caswell <tcaswell@bnl.gov>
2024-01-12 14:49:31 -0500

    MNT: let mongo consumer try to re-subscribe
    Deals with a race between the consumer starting and topic being created.

--- bluesky_config/scripts/mongo_consumer.py
+++ bluesky_config/scripts/mongo_consumer.py
@@ -2,11 +2,14 @@ import argparse
 from functools import partial
 import logging
 from pprint import pprint
+import time
 
 import msgpack
 import msgpack_numpy as mpn
 
 from bluesky_kafka import MongoConsumer
+
+
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger("bluesky.kafka").setLevel("DEBUG")
 
@@ -14,16 +17,22 @@ parser = argparse.ArgumentParser(
     description="monogo consumer process",
 )
 parser.add_argument(
-    "--kafka_server", type=str, help="bootstrap server to connect to.",
-    default="127.0.0.1:9092"
+    "--kafka_server",
+    type=str,
+    help="bootstrap server to connect to.",
+    default="127.0.0.1:9092",
 )
 parser.add_argument(
-    "--kafka_group", type=str, help="bootstrap server to connect to.",
-    default="mongo-consumers"
+    "--kafka_group",
+    type=str,
+    help="bootstrap server to connect to.",
+    default="mongo-consumers",
 )
 parser.add_argument(
-    "--mongo_uri", type=str, help="bootstrap server to connect to.",
-    default="mongodb://localhost:27017"
+    "--mongo_uri",
+    type=str,
+    help="bootstrap server to connect to.",
+    default="mongodb://localhost:27017",
 )
 
 args = parser.parse_args()
@@ -32,7 +41,7 @@ mongo_uri = args.mongo_uri
 bootstrap_servers = args.kafka_server
 
 kafka_deserializer = partial(msgpack.loads, object_hook=mpn.decode)
-auto_offset_reset = "latest"
+auto_offset_reset = "earliest"
 topics = ["^.*bluesky.documents"]
 topic_database_map = {"mad.bluesky.documents": "mad-bluesky-documents"}
 # Create a MongoConsumer that will automatically listen to new beamline topics.
@@ -49,7 +58,18 @@ settings = dict(
     deserializer=kafka_deserializer,
 )
 pprint(settings)
+
 mongo_consumer = MongoConsumer(**settings)
+last_call = 0
 
 
-mongo_consumer.start()
+def work_while_waiting():
+    global last_call
+    if (now := time.monotonic()) > last_call + 10:
+        attached_topics = set(mongo_consumer._consumer.list_topics().topics)
+        if set(topics) - attached_topics:
+            mongo_consumer._consumer.subscribe(topics=topics)
+        last_call = now
+
+
+mongo_consumer.start(None, work_while_waiting)
