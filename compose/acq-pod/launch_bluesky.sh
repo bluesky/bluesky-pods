9f07af7db78d38ed4ebcd59f269438df331b116e
maffettone <pmaffetto@bnl.gov>
2024-07-19 08:49:14 -0700

    fix: caproto connection, needs more attn

--- compose/acq-pod/launch_bluesky.sh
+++ compose/acq-pod/launch_bluesky.sh
@@ -36,6 +36,19 @@ else
 fi
 xauth nlist $LOCAL_DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
+# Get the IP adresses of all caproto IOC containers
+containers=$(podman ps --filter name=caproto --format "{{.Names}}")
+ca_containers=($containers)
+EPICS_CA_ADDR_LIST=""
+
+for container in "${ca_containers[@]}"; do
+    container_ip=$(podman inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container")
+    echo "Adding $container_ip to EPICS_CA_ADDR_LIST"
+    EPICS_CA_ADDR_LIST="$EPICS_CA_ADDR_LIST $container_ip"
+done
+EPICS_CA_ADDR_LIST=$(echo "$EPICS_CA_ADDR_LIST" | xargs)
+EPICS_CA_ADDR_LIST="'$EPICS_CA_ADDR_LIST'"
+
 # https://stackoverflow.com/questions/24112727/relative-paths-based-on-file-location-instead-of-current-working-directory
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
@@ -55,7 +68,7 @@ else
     CMD="ipython3 --ipython-dir=/usr/local/share/ipython"
 fi
 
-
+#TODO (maffettone): fix EPICS_CA_ADDR_LIST
 podman run --pod pod_acq-pod  \
        --net acq-pod_default \
        --network-alias sneaky \
@@ -71,7 +84,7 @@ podman run --pod pod_acq-pod  \
        -v $parent_path/../../bluesky_config/databroker/mad-tiled.yml:/usr/etc/tiled/profiles/mad-tiled.yml \
        -v $parent_path/../../bluesky_config/happi:/usr/local/share/happi \
        -e XDG_RUNTIME_DIR=/tmp/runtime-$USER \
-       -e EPICS_CA_ADDR_LIST=10.0.2.255 \
+       -e EPICS_CA_ADDR_LIST="10.89.0.151 10.89.0.152 10.89.0.153 10.89.0.154 10.89.0.155 10.89.0.156 10.89.0.157" \
        -e EPICS_CA_AUTO_ADDR_LIST=no \
        -e PYTHONPATH=/usr/local/share/ipython\
        -e QSERVER_ZMQ_CONTROL_ADDRESS=tcp://queue_manager:60615\
